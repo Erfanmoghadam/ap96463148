fbbfe8a0d43a2d589fc9306dd4487572494eed04
Erfanmoghadam <44496830+Erfanmoghadam@users.noreply.github.com>
2019-04-25 14:47:19 +0430

    Add files via upload

---
+++ Assignment6.java
@@ -0,0 +1,31 @@
+package assignment6;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        ArrayList<ArrayList<Integer>> twoDimentionArray = new ArrayList<ArrayList<Integer>>();
+
+        Scanner X =new Scanner(System.in);
+        System.out.println("enter the X:");
+            int c = X.nextInt();
+
+        Random rand = new Random();
+            int n = rand.nextInt(c);
+
+            for(int i=0;i<=n;i++){
+
+
+            }
+    }
+}
+
+
+
+
+
+
+
+
+
