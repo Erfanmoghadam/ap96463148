2e1b198abd4c9232236336a6cd80439cdb110a3f
Erfanmoghadam <44496830+Erfanmoghadam@users.noreply.github.com>
2019-05-09 13:53:59 +0430

    Update and rename Assignment4.txt to Assignment4.java

--- Assignment4.java
+++ Assignment4.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void print(int[][] a){
-        for(int i=0;ia.length;i++){
-            for(int j=0;ja.length;j++){
-                System.out.print(a[i][j]+ );
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length;j++){
+                System.out.print(a[i][j]++);
             }
             System.out.println();
         }
     }
     public static void sortRow(int a[][],int r) {
-        if (r  a.length - 1) {
-            System.out.println(Out of index);
+        if (r > a.length - 1) {
+            System.out.println("Out of index");
         } else {
-            for (int n = 0; n  a.length; n++) {
-                for (int i = 0; i  a.length; i++) {
-                    for (int j = i+1; j  a.length; j++) {
-                        if (a[r][i]  a[r][j]) {
+            for (int n = 0; n < a.length; n++) {
+                for (int i = 0; i < a.length; i++) {
+                    for (int j = i+1; j < a.length; j++) {
+                        if (a[r][i] > a[r][j]) {
                             int swap = a[r][i];
                             a[r][i] = a[r][j];
                             a[r][j] = swap;
@@ -30,13 +30,13 @@ public class Main {
         }
     }
     public static void sortColumn(int a[][],int c) {
-        if (c  a.length - 1) {
-            System.out.println(Out of index);
+        if (c > a.length - 1) {
+            System.out.println("Out of index");
         } else {
-            for (int n = 0; n  a.length; n++) {
-                for (int i = 0; i  a.length; i++) {
-                    for (int j = i+1; j  a.length; j++) {
-                        if (a[i][c]  a[j][c]) {
+            for (int n = 0; n < a.length; n++) {
+                for (int i = 0; i < a.length; i++) {
+                    for (int j = i+1; j < a.length; j++) {
+                        if (a[i][c] > a[j][c]) {
                             int swap = a[i][c];
                             a[i][c] = a[j][c];
                             a[j][c] = swap;
@@ -51,8 +51,8 @@ public class Main {
         int m = input.nextInt();
         int a[][] =new int[m][m];
         Random rnd = new Random();
-        for(int i=0;im;i++){
-            for(int j=0;jm;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
                 a[i][j] = rnd.nextInt(100);
             }
         }
@@ -65,7 +65,7 @@ public class Main {
             sortRow(a,Character.getNumericValue(cmd[1]));
             print(a);
         }else{
-            System.out.println(Unkown command);
+            System.out.println("Unkown command");
         }
     }
 }
